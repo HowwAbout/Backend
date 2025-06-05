d206cc7b73eb9364c15e85cc61d621f339eb637f
bak seol <moonleaf0213@naver.com>
2025-06-05 21:50:27 +0900

    refactor: PlanActivityService 내 예외 처리 로직 메서드 분리 및 코드 정리

--- src/main/java/com/threeoh/HowAbout/dateplan/service/PlanActivityService.java
+++ src/main/java/com/threeoh/HowAbout/dateplan/service/PlanActivityService.java
@@ -2,7 +2,6 @@ package com.threeoh.HowAbout.dateplan.service;
 
 import com.threeoh.HowAbout.dateactivity.entity.DateActivity;
 import com.threeoh.HowAbout.dateactivity.repository.DateActivityRepository;
-import com.threeoh.HowAbout.dateactivity.service.DateActivityService;
 import com.threeoh.HowAbout.dateplan.dto.DatePlanResponse;
 import com.threeoh.HowAbout.dateplan.dto.PlanActivityRequest;
 import com.threeoh.HowAbout.dateplan.dto.PlanActivityResponse;
@@ -15,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -27,29 +25,26 @@ public class PlanActivityService {
     private final DateActivityRepository dateActivityRepository;
 
     @Transactional
-    public DatePlanResponse addPlanActivity(Long datePlanId, Long dateActivityId, int order) {
-        DatePlan datePlan = datePlanRepository.findById(datePlanId)
-                .orElseThrow(() -> new IllegalArgumentException("DatePlan not found with DatePlan id : " + datePlanId));
-        DateActivity dateActivity = dateActivityRepository.findById(dateActivityId)
-                .orElseThrow(() -> new IllegalArgumentException("DateActivity not found with DateActivity id : " + dateActivityId));
+    public DatePlanResponse addPlanActivity(Long datePlanId, Long dateActivityId, int sortOrder) {
+        DatePlan datePlan = getDatePlanOrThrow(datePlanId);
+        DateActivity dateActivity = getDateActivityOrThrow(dateActivityId);
 
         // 데이터베이스에서 순서 밀어내기 처리
-        planActivityRepository.incrementOrderForActivities(datePlanId, order);
+        planActivityRepository.incrementOrderForActivities(datePlanId, sortOrder);
 
-        datePlan.addDatePlanActivity(dateActivity, order);
+        datePlan.addDatePlanActivity(dateActivity, sortOrder);
         return DatePlanResponse.from(datePlanRepository.save(datePlan));
     }
 
     @Transactional
     public DatePlanResponse removePlanActivity(Long datePlanId, Long planActivityId) {
-        DatePlan datePlan = datePlanRepository.findById(datePlanId)
-                .orElseThrow(() -> new IllegalArgumentException("DatePlan not found with DatePlan id : " + datePlanId));
+        DatePlan datePlan = getDatePlanOrThrow(datePlanId);
         PlanActivity planActivity = datePlan.getPlanActivities().stream()
                 .filter(onePlanActivity -> onePlanActivity.getId().equals(planActivityId))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("PlanActivity not found with PlanActivity id : " + planActivityId ));
 
-        int removedOrder = planActivity.getOrder();
+        int removedOrder = planActivity.getSortOrder();
         datePlan.removePlanActivity(planActivity);
 
         planActivityRepository.decrementOrderForActivities(datePlanId, removedOrder);
@@ -59,13 +54,12 @@ public class PlanActivityService {
 
     @Transactional
     public PlanActivityResponse updateActivityOrder(Long planActivityId, int newOrder) {
-        PlanActivity planActivity = planActivityRepository.findById(planActivityId)
-                .orElseThrow(() -> new IllegalArgumentException("PlanActivity not found with PlanActivity id : " + planActivityId));
+        PlanActivity planActivity = getPlanActivityOrThrow(planActivityId);
         DatePlan datePlan = planActivity.getDatePlan();
-        int currentOrder = planActivity.getOrder();
+        int currentOrder = planActivity.getSortOrder();
         int maxOrder = planActivityRepository.findMaxOrderByDatePlanId(datePlan.getId());
         if ( newOrder < 1 || newOrder > maxOrder + 1) {
-            throw new IllegalArgumentException("Invalid order value. It must be between 1 and " + (maxOrder + 1));
+            throw new IllegalArgumentException("Invalid sortOrder value. It must be between 1 and " + (maxOrder + 1));
         }
         if (newOrder > currentOrder) {
             planActivityRepository.decrementOrderForRange(datePlan.getId(), currentOrder + 1, newOrder);
@@ -73,7 +67,7 @@ public class PlanActivityService {
             planActivityRepository.incrementOrderForRange(datePlan.getId(), newOrder, currentOrder - 1);
         }
 
-        planActivity.updateOrder(newOrder);
+        planActivity.updateSortOrder(newOrder);
         return PlanActivityResponse.from(planActivityRepository.save(planActivity));
     }
 
@@ -85,25 +79,21 @@ public class PlanActivityService {
 
     @Transactional(readOnly = true)
     public PlanActivityResponse getPlanActivityById(Long planActivityId) {
-        PlanActivity planActivity = planActivityRepository.findById(planActivityId)
-                .orElseThrow(() -> new IllegalArgumentException("PlanActivity not found with PlanActivity id :" + planActivityId));
+        PlanActivity planActivity = getPlanActivityOrThrow(planActivityId);
         return PlanActivityResponse.from(planActivity);
     }
 
     @Transactional
     public PlanActivityResponse updatePlanActivity(Long planActivityId, PlanActivityRequest planActivityRequest) {
-        PlanActivity planActivity = planActivityRepository.findById(planActivityId)
-                .orElseThrow(() -> new IllegalArgumentException("PlanActivity not found with PlanActivity id : " + planActivityId));
+        PlanActivity planActivity = getPlanActivityOrThrow(planActivityId);
 
-        DateActivity dateActivity = dateActivityRepository.findById(planActivityRequest.dateActivityId())
-                .orElseThrow(() -> new IllegalArgumentException("DateActivity not found with DateActivity id : " + planActivityRequest.dateActivityId()));
-
+        DateActivity dateActivity = getDateActivityOrThrow(planActivityRequest.dateActivityId());
         DatePlan datePlan = planActivity.getDatePlan();
-        int newOrder = planActivityRequest.order();
-        int currentOrder = planActivity.getOrder();
+        int newOrder = planActivityRequest.sortOrder();
+        int currentOrder = planActivity.getSortOrder();
         int maxOrder = planActivityRepository.findMaxOrderByDatePlanId(datePlan.getId());
         if ( newOrder < 1 || newOrder > maxOrder + 1) {
-            throw new IllegalArgumentException("Invalid order value. It must be between 1 and " + (maxOrder + 1));
+            throw new IllegalArgumentException("Invalid sortOrder value. It must be between 1 and " + (maxOrder + 1));
         }
         if (newOrder > currentOrder) {
             planActivityRepository.decrementOrderForRange(datePlan.getId(), currentOrder + 1, newOrder);
@@ -111,15 +101,26 @@ public class PlanActivityService {
             planActivityRepository.incrementOrderForRange(datePlan.getId(), newOrder, currentOrder - 1);
         }
 
-        planActivity.updateOrder(newOrder);
+        planActivity.updateSortOrder(newOrder);
 
-        planActivity.updatePlanActivity(dateActivity, planActivityRequest.order());
+        planActivity.updatePlanActivity(dateActivity, planActivityRequest.sortOrder());
 
         return PlanActivityResponse.from(planActivityRepository.save(planActivity));
     }
 
+    private DatePlan getDatePlanOrThrow(Long datePlanId) {
+        return datePlanRepository.findById(datePlanId)
+                .orElseThrow(() -> new IllegalArgumentException("DatePlan not found with id: " + datePlanId));
+    }
 
+    private PlanActivity getPlanActivityOrThrow(Long planActivityId) {
+        return planActivityRepository.findById(planActivityId)
+                .orElseThrow(() -> new IllegalArgumentException("PlanActivity not found with id: " + planActivityId));
+    }
 
-
+    private DateActivity getDateActivityOrThrow(Long dateActivityId) {
+        return dateActivityRepository.findById(dateActivityId)
+                .orElseThrow(() -> new IllegalArgumentException("DateActivity not found with id: " + dateActivityId));
+    }
 
 }
